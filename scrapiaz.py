0d04328515c299419ee8e142ba14dea7a8dee3df
Johnny Chang <johnny@johnnychang.com>
2015-10-04 00:26:26 -0500

    added fetching songlist script tag from url

--- scrapiaz.py
+++ scrapiaz.py
@@ -2,13 +2,32 @@
 import filey, urlz
 from bs4 import BeautifulSoup
 
-test_url = "http://www.azlyrics.com/g/ghostfacekillah.html"
+test_url = "http://www.azlyrics.com/j/jayz.html"
 
 def read_in_urls(url_filename):
-"""Reads in urls from a file of urls, one on each line."""
+    """Reads in urls from a file of urls, one on each line."""
     if len(url_filename) == 0:
         raise Exception("File name is length 0")
 
     return filey.read_from_file("", url_filename)
 
+def find_songlist_tag(soup):
+    songlist_tag = ""
+    for tag in soup.findAll("script"):
+        tag_text = tag.text
+        if "var songlist" in tag_text:
+            songlist_tag = tag_text
+            break
+
+    return songlist_tag
+
+def scrape_url(url):
+    page = urlz.open_and_read(url)
+    soup = BeautifulSoup(page, "lxml")
+
+    songlist_tag = find_songlist_tag(soup)
+
+    print songlist_tag
+
 #print read_in_urls("urls.txt")
+scrape_url(test_url)
