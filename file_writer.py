2c82d733125bed92bb39852201edca791d1e320f
Johnny Chang <johnny@johnnychang.com>
2015-10-03 23:36:25 -0500

    FEATURE: added file creation and writing list to file with newline

--- file_writer.py
+++ file_writer.py
@@ -2,6 +2,8 @@
 
 import os, sys, errno
 
+# Tries to make path and only catches exceptions not relating to directory
+# already existing.
 def make_path(path):
     try:
         os.makedirs(path)
@@ -10,8 +12,12 @@ def make_path(path):
             raise
 
 def write_to_file(path, filename, data):
-    # Recursively create path passed in
+    # Recursively create path passed as arg.
     make_path(path)
 
     with open(os.path.join(path, filename), "wb") as file_target:
-        file_target.write(data)
+        if type(data) is list:
+            for line in data:
+                file_target.write(line + "\n")
+        elif type(data) is str:
+            file_target.write(data)
--- main.py
+++ main.py
@@ -3,3 +3,6 @@
 import file_writer
 
 file_writer.make_path("fuck/my/shit")
+
+hoopla = ["david", "dalpiaz", "is", "my", "advisor"]
+file_writer.write_to_file("fuck/my/shit", "sherry.txt", hoopla)
