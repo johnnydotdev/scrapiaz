95d4ef123692487ab4e153628a77a608736118f1
Johnny Chang <johnny@johnnychang.com>
2015-10-06 15:57:54 -0500

    changed code formatting and added comments

--- scrapiaz.py
+++ scrapiaz.py
@@ -3,9 +3,8 @@ import filey, urlz, string, song, os, urlparse, random
 from bs4 import BeautifulSoup
 from time import sleep
 
-test_url = "http://www.azlyrics.com/j/jayz.html"
 DATA_PATH = "data"
-SONG_EXT = ".txt"
+SONG_EXT  = ".txt"
 
 def read_in_urls(url_filename):
     """Reads in urls from a file of urls, one on each line."""
@@ -15,6 +14,11 @@ def read_in_urls(url_filename):
     return filey.read_from_file("", url_filename)
 
 def find_songlist_tag(soup):
+    """
+    In the page source code, there exists a variable called songlist.
+    songlist is an array of objects containing the song name and URL.
+    This subroutine finds the songlist tag and returns the text associated with it.
+    """
     songlist_tag = ""
     for tag in soup.findAll("script"):
         tag_text = tag.text
@@ -35,7 +39,7 @@ def split_json_elem(json_elem):
 
     if len(properties) > 1:
         song_title = properties[0].lstrip("s: \"").rstrip("\"")
-        song_url = properties[1]
+        song_url   = properties[1]
         if song_url.startswith("h:\""):
             song_url = properties[1][3:]
         song_url = song_url.rstrip("\"")
@@ -49,8 +53,9 @@ def decode_json(json_string):
     Removes braces, calls split_json_elem on the contents.
     Returns a list of parsed Song objects.
     """
-    elems = json_string.lstrip("\r {").rstrip("}").split("}, {")
+    elems     = json_string.lstrip("\r {").rstrip("}").split("}, {")
     song_list = []
+
     for elem in elems:
         song_list.append(split_json_elem(elem))
 
@@ -62,22 +67,33 @@ def print_song_list(song_list):
         print "Song Name: %-*s URL: %s"  % (40, song.name, song.url)
 
 def get_name_from_url(url):
+    """Gets artist or song name from the URL."""
     return url.split("/")[-1][0:-5]
 
 def get_song_lyrics(parsed_url, url):
+    """
+    Takes in two urls--sometimes, with artist collaborations, the second url is
+    not a relative URL, but a complete one. I use urlparse to join the two urls
+    such that they are baseURL invariant.
+
+    Returns the song lyrics, and prints "Success!" if successful.
+    """
     if url.startswith(".."):
         url = urlparse.urljoin(parsed_url.geturl(), url)
     print "Parsing %s..." % (url)
-    page = urlz.open_and_read(url)
-    soup = BeautifulSoup(page, "lxml")
+
+    page      = urlz.open_and_read(url)
+    soup      = BeautifulSoup(page, "lxml")
     page_text = soup.get_text()
-    lyrics = page_text[(page_text.index("Print") + 5):page_text.index("if  ( /Android")].strip("\n")
+    lyrics    = page_text[(page_text.index("Print") + 5):page_text.index("if  ( /Android")].strip("\n")
+
     if type(lyrics) == None:
         print "Nothing returned."
     elif len(lyrics) > 0:
         print "Success!"
     else:
         print "Lyrics are empty."
+
     return lyrics
 
 def write_songs(artist, song_list, parsed_url):
@@ -85,12 +101,13 @@ def write_songs(artist, song_list, parsed_url):
     count = 0
     selected_indices = []
     while count < length:
+        # Find a random index from the song_list and ensure it is new.
         rand_idx = random.randint(0, length - 1)
         while rand_idx in selected_indices:
             rand_idx = random.randint(0, length - 1)
-        song = song_list[rand_idx]
 
-        song_path = os.path.join(DATA_PATH, artist)
+        song           = song_list[rand_idx]
+        song_path      = os.path.join(DATA_PATH, artist)
         song_file_name = filey.scrub_file_name(song.name + SONG_EXT)
 
         if filey.already_exists(song_path, song_file_name):
@@ -112,10 +129,10 @@ def scrape_artist_for_song_urls(url):
     Take in a url, opens it, and parses the page for song URLs and names.
     """
     try:
-        parsed_url = urlparse.urlparse(url)
-        artist     = get_name_from_url(url)
-        page       = urlz.open_and_read(url)
-        soup       = BeautifulSoup(page, "lxml")
+        parsed_url   = urlparse.urlparse(url)
+        artist       = get_name_from_url(url)
+        page         = urlz.open_and_read(url)
+        soup         = BeautifulSoup(page, "lxml")
 
         songlist_tag = string.join(find_songlist_tag(soup).split("}];")[0].split("\n")[1:]).strip()
         json_arg     = "{%s}" % (songlist_tag.split("[", 1)[1].rsplit("]", 1)[0].lstrip(" "))
